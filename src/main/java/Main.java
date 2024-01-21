4d74e2a1e0c99dba709399e5985a677e794bce55
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 23:35:36 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+package com.ejemplo;
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.setVisible(true);
+    }
 }
